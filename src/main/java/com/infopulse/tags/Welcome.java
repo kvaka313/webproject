1acf8a0aaa2faf391f9326316863d3acd87ce01a
galkin <oleksandr.galkin@intapp.com>
2018-11-10 16:10:00 +0200

    custom tags

--- src/main/java/com/infopulse/tags/Welcome.java
+++ src/main/java/com/infopulse/tags/Welcome.java
@@ -2,18 +2,43 @@ package com.infopulse.tags;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.BodyTagSupport;
 import javax.servlet.jsp.tagext.TagSupport;
 import java.io.IOException;
 
-public class Welcome extends TagSupport {
+public class Welcome extends BodyTagSupport {
+    private String name;
+
+    @Override
     public int doStartTag() throws JspException {
         try{
             JspWriter out = pageContext.getOut();
-            out.println("Hello, all");
+            out.println("Hello, "+name);
         }
         catch( IOException ioException ) {
             throw new JspException( ioException.getMessage() );
         }
-        return SKIP_BODY;
+        return EVAL_BODY_BUFFERED;
+    }
+
+
+    @Override
+    public int doEndTag(){
+        String body = getBodyContent().getString();
+        JspWriter out = pageContext.getOut();
+        try {
+            out.println(body);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return EVAL_PAGE;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,7 @@
 
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" import="java.util.*" %>
 <%@ taglib uri = "/WEB-INF/tld/mytag.tld" prefix = "mytag" %>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <html>
   <head>
     <title></title>
@@ -12,6 +13,11 @@
       <input type="submit" name="ok" value="send"/>
 
     </form>
-   <mytag:welcome/>
+    <mytag:welcome name="vasya">
+       ew7teuwteywtewyetwyuteuytewytew
+       daywqtywtwyeteywteytewytdeydteytdey
+    </mytag:welcome>
+    <c:set var="myvar" value="jjjjjjjjj"/>
+    <c:out value="${myvar}"/>
   </body>
 </html>
